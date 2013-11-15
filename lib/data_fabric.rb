1c8e3d1b8f1c5f7bae1819cc131cb2f2452526ab
Michael Kuehl <michael@expectedbehavior.com>
2013-11-15 09:36:13 -0500

    add activate_shards helper wrapper

--- lib/data_fabric.rb
+++ lib/data_fabric.rb
@@ -6,33 +6,33 @@ require File.expand_path('../data_fabric/version', __FILE__)
 
 # DataFabric adds a new level of flexibility to ActiveRecord connection handling.
 # You need to describe the topology for your database infrastructure in your model(s).  As with ActiveRecord normally, different models can use different topologies.
-# 
+#
 # class MyHugeVolumeOfDataModel < ActiveRecord::Base
 #   data_fabric :replicated => true, :shard_by => :city
 # end
-# 
+#
 # There are four supported modes of operation, depending on the options given to the data_fabric method.  The plugin will look for connections in your config/database.yml with the following convention:
-# 
+#
 # No connection topology:
 # #{environment} - this is the default, as with ActiveRecord, e.g. "production"
-# 
+#
 # data_fabric :replicated => true
 # #{environment}_#{role} - no sharding, just replication, where role is "master" or "slave", e.g. "production_master"
-# 
+#
 # data_fabric :shard_by => :city
 # #{group}_#{shard}_#{environment} - sharding, no replication, e.g. "city_austin_production"
-# 
+#
 # data_fabric :replicated => true, :shard_by => :city
 # #{group}_#{shard}_#{environment}_#{role} - sharding with replication, e.g. "city_austin_production_master"
-# 
-# 
+#
+#
 # When marked as replicated, all write and transactional operations for the model go to the master, whereas read operations go to the slave.
-# 
+#
 # Since sharding is an application-level concern, your application must set the shard to use based on the current request or environment.  The current shard for a group is set on a thread local variable.  For example, you can set the shard in an ActionController around_filter based on the user as follows:
-# 
+#
 # class ApplicationController < ActionController::Base
 #   around_filter :select_shard
-#   
+#
 #   private
 #   def select_shard(&action_block)
 #     DataFabric.activate_shard(:city => @current_user.city, &action_block)
@@ -41,13 +41,13 @@ require File.expand_path('../data_fabric/version', __FILE__)
 module DataFabric
 
   def self.logger
-    @logger ||= ActiveRecord::Base.logger || default_logger 
+    @logger ||= ActiveRecord::Base.logger || default_logger
   end
-  
+
   def self.logger=(log)
     @logger = log
   end
-  
+
   def self.activate_shard(shards, &block)
     ensure_setup
 
@@ -65,7 +65,13 @@ module DataFabric
       end
     end
   end
-  
+
+  def self.activate_shards(shard_strategies, shard_id, &block)
+    activate_shard(Hash[shard_strategies.map{|ss| [ss, shard_id]}]) do
+      yield
+    end
+  end
+
   # For cases where you can't pass a block to activate_shards, you can
   # clean up the thread local settings by calling this method at the
   # end of processing
@@ -75,7 +81,7 @@ module DataFabric
       Thread.current[:shards].delete(key.to_s)
     end
   end
-  
+
   def self.active_shard(group)
     raise ArgumentError, 'No shard has been activated' unless Thread.current[:shards]
 
@@ -83,7 +89,7 @@ module DataFabric
     raise ArgumentError, "No active shard for #{group}" unless shard
     shard
   end
-  
+
   def self.shard_active_for?(group)
     return true unless group
     Thread.current[:shards] and Thread.current[:shards][group.to_s]
